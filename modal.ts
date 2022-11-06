71d0b996cd57e6d5fce1c0a48a7efd5206e1fa80
ennioitaliano <ennio.italiano@gmail.com>
2022-11-06 18:03:52 +0100

    Started working on this Dataview feature - implemented it in the modal only - added descriptions in the modal for clarity - had to decide if it makes sense to continue developing this feature

--- modal.ts
+++ modal.ts
@@ -2,18 +2,20 @@ import { App, Modal, Notice, Setting } from "obsidian";
 import { fileExists } from "utils";
 
 export class CreateFileModal extends Modal {
+	option: string;
 	fileName: string;
 	chosenFolder: string;
-	option: string;
+	fileOrder: string;
+	destFolder: string;
+	dvField: string;
 	onSubmit: (
 		option: string,
 		fileName?: string,
 		chosenFolder?: string,
 		fileOrder?: string,
-		destFolder?: string
+		destFolder?: string,
+		dvField?: string
 	) => void;
-	fileOrder: string;
-	destFolder: string;
 
 	constructor(
 		app: App,
@@ -22,11 +24,12 @@ export class CreateFileModal extends Modal {
 			fileName: string,
 			chosenFolder: string,
 			fileOrder: string,
-			destFolder: string
+			destFolder: string,
+			dvField: string
 		) => void,
 		passedFolder?: string,
 		passedName?: string,
-		passedOption?: string,
+		passedOption?: string
 	) {
 		super(app);
 		this.onSubmit = onSubmit;
@@ -40,7 +43,7 @@ export class CreateFileModal extends Modal {
 
 		contentEl.createEl("h1", { text: "AutoGlossary" });
 
-		new Setting(contentEl).setName("Folder").addText((text) =>
+		new Setting(contentEl).setName("Folder").setDesc("The folder to get the files indexed from.").addText((text) =>
 			text
 				.onChange((value) => {
 					this.chosenFolder = value;
@@ -48,10 +51,10 @@ export class CreateFileModal extends Modal {
 				.setValue(this.chosenFolder)
 		);
 
-		new Setting(contentEl).setName("Same destination").addToggle((toggle) =>
+		new Setting(contentEl).setName("Same destination as folder").setDesc("If on, the file will be created in the same folder specified above and the 'Destination' field will be disabled.").addToggle((toggle) =>
 			toggle.setValue(true).onChange((value) => {
-				destination.settingEl.toggleVisibility(!value);
-				if(value) {
+				destination.setDisabled(value);
+				if (value) {
 					this.destFolder = this.chosenFolder;
 				}
 			})
@@ -61,16 +64,18 @@ export class CreateFileModal extends Modal {
 
 		destination
 			.setName("Destination")
+			.setDesc("If the toggle above is on, specify here the destination folder for the file created.")
 			.addText((text) =>
 				text
 					.onChange((value) => {
 						this.destFolder = value;
 					})
 					.setValue(this.destFolder)
+					.setDisabled(true)
 			)
-			.settingEl.toggleVisibility(false);
+			.setDisabled(true);
 
-		new Setting(contentEl).setName("File name").addText((text) =>
+		new Setting(contentEl).setName("File name").setDesc("The name of the created file.").addText((text) =>
 			text
 				.onChange((value) => {
 					this.fileName = value;
@@ -78,7 +83,7 @@ export class CreateFileModal extends Modal {
 				.setValue(this.fileName)
 		);
 
-		new Setting(contentEl).setName("File order").addDropdown((drop) =>
+		new Setting(contentEl).setName("File order").setDesc("The order for the files to be indexed.").addDropdown((drop) =>
 			drop
 				.addOption("", "File order")
 				.addOption("default", "Default")
@@ -88,12 +93,13 @@ export class CreateFileModal extends Modal {
 				.addOption("ctime_old", "Creation time - Oldest to newest")
 				.addOption("alphabetical", "Alphabetical")
 				.addOption("alphabetical_rev", "Alphabetical - Reverse")
+				.setValue("default")
 				.onChange((chosen) => {
 					this.fileOrder = chosen;
 				})
 		);
 
-		new Setting(contentEl).setName("File type").addDropdown((drop) =>
+		new Setting(contentEl).setName("File type").setDesc("Choose between index, glossary or both.").addDropdown((drop) =>
 			drop
 				.addOption("", "File type") // have to do this in order to get some value != undefined
 				.addOption("glossary", "Glossary")
@@ -104,6 +110,25 @@ export class CreateFileModal extends Modal {
 				})
 				.setValue(this.option)
 		);
+
+		new Setting(contentEl).setName("Dataview").setDesc("If on, you can create the file using Dataview queries.").addToggle((toggle) =>
+			toggle.setValue(false).onChange((value) => {
+				dvfield.setDisabled(!value);
+			})
+		);
+
+		let dvfield = new Setting(contentEl);
+
+		dvfield
+			.setName("Dataview field")
+			.addText((text) =>
+				text
+					.onChange((value) => {
+						this.dvField = value;
+					})
+					.setValue(this.dvField)
+			)
+			.setDisabled(true);
 
 		new Setting(contentEl).addButton((btn) =>
 			btn
