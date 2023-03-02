3f218598f60b5322acdcbef121bae033c34f5e84
ennio <ennio.italiano@gmail.com>
2023-03-02 12:14:26 +0100

    Moved settings to a separate file

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import { App, Plugin, PluginSettingTab, Setting, TFolder } from "obsidian";
+import { Plugin, TFolder } from "obsidian";
 
 import { CreateFileModal } from "./modal";
 import { createFile } from "./glossaryIndex";
 import { getEnumFT, getEnumFO, fileType } from "./utils";
-
-interface AutoGlossarySettings {
-	fileInclusion: boolean;
-	sameDest: boolean;
-	fileDest: string;
-	fileOverwrite: boolean;
-	fileOrder: string;
-}
-
-const DEFAULT_SETTINGS: AutoGlossarySettings = {
-	fileInclusion: false,
-	sameDest: true,
-	fileDest: "",
-	fileOverwrite: false,
-	fileOrder: "default",
-};
+import { AutoGlossarySettings, DEFAULT_SETTINGS, SettingTab } from "settings";
 
 export default class autoGlossary extends Plugin {
 	// SETTINGS
@@ -290,116 +275,5 @@ export default class autoGlossary extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 		console.log("Settings saved.");
-	}
-}
-
-// SETTINGS
-class SettingTab extends PluginSettingTab {
-	plugin: autoGlossary;
-
-	constructor(app: App, plugin: autoGlossary) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		containerEl.createEl("h2", { text: "Auto Glossary Settings" });
-
-		new Setting(containerEl)
-			.setName("File inclusion")
-			.setDesc(
-				"Include previously generated files in glossaries and indexes."
-			)
-			.addToggle((toggle) =>
-				toggle
-					.setValue(this.plugin.settings.fileInclusion)
-					.onChange(async (value) => {
-						console.log("fileInclusion switched to " + value);
-						this.plugin.settings.fileInclusion = value;
-						await this.plugin.saveSettings();
-					})
-			);
-
-		containerEl.createEl("h3", { text: "Default options" });
-
-		new Setting(containerEl)
-			.setName("Same destination as folder")
-			.setDesc(
-				"If on, files will be created in the same folder specified above and the 'Destination' field will be disabled."
-			)
-			.addToggle((toggle) =>
-				toggle
-					.setValue(this.plugin.settings.sameDest)
-					.onChange(async (value) => {
-						console.log("sameDest switched to " + value);
-						this.plugin.settings.sameDest = value;
-						destination.setDisabled(value);
-						await this.plugin.saveSettings();
-					})
-			);
-
-		const destination = new Setting(containerEl);
-
-		destination
-			.setName("Destination")
-			.setDesc(
-				"If the above toggle is off, specify here the destination folder for the files created."
-			)
-			.addText((text) =>
-				text
-					.onChange(async (value) => {
-						this.plugin.settings.fileDest = value;
-						console.log("fileDest switched to " + value);
-						await this.plugin.saveSettings();
-					})
-					.setValue(this.plugin.settings.fileDest)
-					.setDisabled(true)
-			)
-			.setDisabled(this.plugin.settings.sameDest);
-
-		new Setting(containerEl)
-			.setName("Overwrite existing files")
-			.setDesc(
-				"Set the default behavior when a file already exists. Can be changed every time in the modal."
-			)
-			.addToggle((toggle) =>
-				toggle
-					.setValue(this.plugin.settings.fileOverwrite)
-					.onChange(async (value) => {
-						console.log("fileOverwrite switched to " + value);
-						this.plugin.settings.fileOverwrite = value;
-						await this.plugin.saveSettings();
-					})
-			);
-
-		new Setting(containerEl)
-			.setName("File order")
-			.setDesc("The order for the files to be indexed.")
-			.addDropdown((drop) =>
-				drop
-					.addOption("default", "Default")
-					.addOption(
-						"mtime_new",
-						"Modification time - Newest to oldest"
-					)
-					.addOption(
-						"mtime_old",
-						"Modification time - Oldest to newest"
-					)
-					.addOption("ctime_new", "Creation time - Newest to oldest")
-					.addOption("ctime_old", "Creation time - Oldest to newest")
-					.addOption("alphabetical", "Alphabetical")
-					.addOption("alphabetical_rev", "Alphabetical - Reverse")
-					.setValue(this.plugin.settings.fileOrder)
-					.onChange(async (chosen) => {
-						console.log("fileOrder switched to " + chosen);
-						this.plugin.settings.fileOrder = chosen;
-						await this.plugin.saveSettings();
-					})
-			);
 	}
 }
---
+++ src/settings.ts
@@ -0,0 +1,128 @@
+import autoGlossary from "main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+
+export interface AutoGlossarySettings {
+	fileInclusion: boolean;
+	sameDest: boolean;
+	fileDest: string;
+	fileOverwrite: boolean;
+	fileOrder: string;
+}
+
+export const DEFAULT_SETTINGS: AutoGlossarySettings = {
+	fileInclusion: false,
+	sameDest: true,
+	fileDest: "",
+	fileOverwrite: false,
+	fileOrder: "default",
+};
+
+export class SettingTab extends PluginSettingTab {
+	plugin: autoGlossary;
+
+	constructor(app: App, plugin: autoGlossary) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "Auto Glossary Settings" });
+
+		new Setting(containerEl)
+			.setName("File inclusion")
+			.setDesc(
+				"Include previously generated files in glossaries and indexes."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.fileInclusion)
+					.onChange(async (value) => {
+						console.log("fileInclusion switched to " + value);
+						this.plugin.settings.fileInclusion = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		containerEl.createEl("h3", { text: "Default options" });
+
+		new Setting(containerEl)
+			.setName("Same destination as folder")
+			.setDesc(
+				"If on, files will be created in the same folder specified above and the 'Destination' field will be disabled."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.sameDest)
+					.onChange(async (value) => {
+						console.log("sameDest switched to " + value);
+						this.plugin.settings.sameDest = value;
+						destination.setDisabled(value);
+						await this.plugin.saveSettings();
+					})
+			);
+
+		const destination = new Setting(containerEl);
+
+		destination
+			.setName("Destination")
+			.setDesc(
+				"If the above toggle is off, specify here the destination folder for the files created."
+			)
+			.addText((destText) =>
+				destText
+					.onChange(async (value) => {
+						this.plugin.settings.fileDest = value;
+						console.log("fileDest switched to " + value);
+						await this.plugin.saveSettings();
+					})
+					.setValue(this.plugin.settings.fileDest)
+					.setDisabled(true)
+			)
+			.setDisabled(this.plugin.settings.sameDest);
+
+		new Setting(containerEl)
+			.setName("Overwrite existing files")
+			.setDesc(
+				"Set the default behavior when a file already exists. Can be changed every time in the modal."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.fileOverwrite)
+					.onChange(async (value) => {
+						console.log("fileOverwrite switched to " + value);
+						this.plugin.settings.fileOverwrite = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("File order")
+			.setDesc("The order for the files to be indexed.")
+			.addDropdown((drop) =>
+				drop
+					.addOption("default", "Default")
+					.addOption(
+						"mtime_new",
+						"Modification time - Newest to oldest"
+					)
+					.addOption(
+						"mtime_old",
+						"Modification time - Oldest to newest"
+					)
+					.addOption("ctime_new", "Creation time - Newest to oldest")
+					.addOption("ctime_old", "Creation time - Oldest to newest")
+					.addOption("alphabetical", "Alphabetical")
+					.addOption("alphabetical_rev", "Alphabetical - Reverse")
+					.setValue(this.plugin.settings.fileOrder)
+					.onChange(async (chosen) => {
+						console.log("fileOrder switched to " + chosen);
+						this.plugin.settings.fileOrder = chosen;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
